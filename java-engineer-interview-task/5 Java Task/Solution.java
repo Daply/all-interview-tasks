d47fa46136e27073afff719f167f5ebd0b9b35d8
Daply <daply96@gmail.com>
2021-01-08 17:36:29 +0300

    Added new interview task for java engineer

---
+++ java-engineer-interview-task/5 Java Task/Solution.java
@@ -0,0 +1,50 @@
+package stockmarketprediction;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int stockDataCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> stockData = IntStream.range(0, stockDataCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int queriesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> queries = IntStream.range(0, queriesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.predictAnswer(stockData, queries);
+        System.out.println(result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+                + "\n");
+
+        bufferedReader.close();
+        //bufferedWriter.close();
+    }
+}
